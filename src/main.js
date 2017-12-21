52445dc3b9eba5ab328c64dd48e420572c8d199e
Pitu <7425261+Pitu@users.noreply.github.com>
2017-12-21 01:26:00 -0300

    Added vuebar for nice scrolling bars

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
+import Vuebar from 'vuebar';
 import VueScrollTo from 'vue-scrollto';
 
+Vue.use(Vuebar);
 Vue.use(VueScrollTo);
+
+let appendableElement = null;
+
+function prepareDOM() {
+	const maganeContainer = document.createElement('div');
+	maganeContainer.id = 'maganeContainer';
+	appendableElement.appendChild(maganeContainer);
+
+	new Vue({
+		el: '#maganeContainer',
+		render: h => h(App)
+	});
+}
 
 let loadTimer = setInterval(() => {
 	appendableElement = document.querySelector('[class^="channelTextArea"] [class^="inner"]')
@@ -12,19 +27,3 @@ let loadTimer = setInterval(() => {
 		prepareDOM();
 	}
 }, 1000);
-
-const elemName = 'maganeContainer';
-let appendableElement = null;
-let maganeContainer = null;
-let lastKnownLocation = null;
-
-function prepareDOM() {
-	maganeContainer = document.createElement('div');
-	maganeContainer.id = elemName;
-	appendableElement.appendChild(maganeContainer);
-
-	new Vue({
-		el: `#${elemName}`,
-		render: h => h(App)
-	});
-}
