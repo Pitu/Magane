46a09ef60729fbac2477d4abb65f5baa7e271a91
Pitu <heyitspitu@gmail.com>
2020-01-02 21:51:29 +0900

    update dependencies and remove sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,22 +19,6 @@ module.exports = {
 				]
 			},
 			{
-				test: /\.scss$/,
-				use: [
-					'vue-style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			},
-			{
-				test: /\.sass$/,
-				use: [
-					'vue-style-loader',
-					'css-loader',
-					'sass-loader?indentedSyntax'
-				]
-			},
-			{
 				test: /\.vue$/,
 				loader: 'vue-loader',
 				options: {
