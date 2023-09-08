48ea272158338aa2374ad953e05b173013ced3f2
Bobby <bobby@fiery.me>
2023-09-09 00:03:20 +0700

    fix: classic rollup script
    and make the other rollup scripts a bit more readable

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,13 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import command from 'rollup-plugin-command';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import postcssPresetEnv from 'postcss-preset-env';
 import serve from 'rollup-plugin-serve';
 import autoPreprocess from 'svelte-preprocess';
+import fs from 'fs/promises';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -48,10 +48,16 @@ export default {
 			}
 		}),
 		!production && livereload('public'),
-		!production && Boolean(process.env.BD_PLUGIN_PATH) &&
-			command(`touch "${process.env.BD_PLUGIN_PATH}"`, {
-				wait: true
-			}),
+		{
+			name: 'utimesPluginFile',
+			writeBundle: async () => {
+				if (production || !Boolean(process.env.BD_PLUGIN_PATH)) return;
+				// Updating timestamps of the plugin file will notify BetterDiscord to automatically reload it
+				const now = new Date();
+				await fs.utimes(process.env.BD_PLUGIN_PATH, now, now);
+				console.log(`Updated timestamps of ${process.env.BD_PLUGIN_PATH}`);
+			}
+		},
 		production && terser()
 	],
 	watch: {
